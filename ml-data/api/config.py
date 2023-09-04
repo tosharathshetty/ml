e7687f66fc88b21d485a9374235c8da89d4db144
tosharathshetty <to.sharath.shetty@gmail.com>
2023-09-04 15:06:33 -0500

    first commit

---
+++ ml-data/api/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    huggingface_key: str = "replace_huggigface_key"
+    sparrow_key: str = ""
+    dataset_name: str = "katanaml-org/invoices-donut-data-v1"
+    ocr_stats_file: str = "data/ocr_stats.json"
+
+
+settings = Settings()
